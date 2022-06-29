378ce3a8f872faa4d9ab3ce71b2ee8dab53c1bfd
Matteo <matteorascioni@gmail.com>
2022-06-29 18:16:59 +0200

    cleaning up the cart after fetching

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import React, { useEffect } from 'react';
+
+import React from 'react';
+import { Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { sendCartData, fetchCartData } from './store/cart/cart-actions';
-import Notification from './components/Notification/Notification';
-import Cart from './components/Cart/Cart'
+import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
-import Products from './components/Product/Product';
+import Product from './components/Product/Product';
+import Notification from './components/Notification/Notification';
+import { sendCartData, fetchCartData } from './store/cart/cart-actions';
 
 let isInitial = true;
 
 const App = () => {
   const dispatch = useDispatch();
-  const cart = useSelector (state => state.cart);
-  const showCart = useSelector(state => state.ui.cartIsVisible);
-  const notification = useSelector(state => state.ui.notification);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
     dispatch(fetchCartData());
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
-      if(isInitial) {
-        isInitial = false;
-        return;
-      }
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
 
-      if(cart.changed) {
-        dispatch(sendCartData(cart));
-      }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
-    <>
+    <Fragment>
       {notification && (
-        <Notification 
+        <Notification
           status={notification.status}
-          title={notification.title} 
+          title={notification.title}
           message={notification.message}
         />
       )}
       <Layout>
         {showCart && <Cart />}
-        <Products />
+        <Product />
       </Layout>
-    </>
+    </Fragment>
   );
 }
 
